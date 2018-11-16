2d8b5123fcd48374df95fc5226ff87374976857d
Viktor Yanush <yanushviktor@gmail.com>
2018-11-16 17:38:29 +0300

    Samplers (#6)
    * Added class for samplers and new samplers: (R)SGLD, (R)HMC, SG(R)HMC
    
    * fixed typos in RHMC
    
    * Removed duplication and moved everything to one file
    
    * Removed duplication and moved everything to one file
    
    * reversibility test for RHMC
    
    * Moved mixin to base class; Reformated code
    
    * new test for RHMC on the normal distribution
    
    * Now we test sampling on standard normal for RSGLD and SGRHMC also

---
+++ tests/test_rhmc.py
@@ -0,0 +1,107 @@
+import geoopt
+import torch
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        dict(epsilon=1e-3, n_steps=1),
+        dict(epsilon=1e-4, n_steps=1),
+        dict(epsilon=1e-3, n_steps=2),
+        dict(epsilon=1e-4, n_steps=2),
+        dict(epsilon=1e-3, n_steps=3),
+        dict(epsilon=1e-4, n_steps=3),
+        dict(epsilon=1e-4, n_steps=10),
+        dict(epsilon=1e-5, n_steps=10),
+    ],
+)
+def test_leapfrog_reversibility(params):
+    torch.set_default_dtype(torch.float64)
+
+    class NormalDist(torch.nn.Module):
+        def __init__(self, mu, sigma):
+            super().__init__()
+            self.d = torch.distributions.Normal(mu, sigma)
+            self.x = torch.nn.Parameter(torch.randn_like(mu))
+
+        
+        def forward(self):
+            return self.d.log_prob(self.x).sum()
+
+    epsilon, n_steps = params['epsilon'], params['n_steps']
+
+    torch.manual_seed(42)
+    nd = NormalDist(torch.randn([10]), torch.ones([10]))
+
+    init_x = nd.x.data.numpy().copy()
+
+    torch.manual_seed(42)
+    sampler = geoopt.samplers.RHMC(nd.parameters(), **params)
+
+    r = torch.randn([10])
+
+    for i in range(n_steps):
+        logp = nd()
+        logp.backward()
+        sampler._step(nd.x, r, epsilon)
+        nd.x.grad.zero_()
+    
+    for i in range(n_steps):
+        logp = nd()
+        logp.backward()
+        sampler._step(nd.x, r, -epsilon)
+        nd.x.grad.zero_()
+
+    new_x = nd.x.data.numpy().copy()
+    np.testing.assert_allclose(init_x, new_x, rtol=1e-5)
+
+
+@pytest.mark.parametrize(
+    "params",
+    [
+        dict(sampler='RHMC'  , epsilon=0.2, n_steps=5, n_burn=1000, n_samples=5000),
+        dict(sampler='RSGLD' , epsilon=1e-3, n_burn=3000, n_samples=10000),
+        dict(sampler='SGRHMC', epsilon=1e-3, n_steps=1, alpha=0.5, n_burn=3000, n_samples=10000)
+    ],
+)
+def test_sampling(params):
+    class NormalDist(torch.nn.Module):
+        def __init__(self, mu, sigma):
+            super().__init__()
+            self.d = torch.distributions.Normal(mu, sigma)
+            self.x = torch.nn.Parameter(torch.randn_like(mu))
+
+        
+        def forward(self):
+            return self.d.log_prob(self.x).sum()
+    
+    torch.manual_seed(42)
+    D = 2
+    n_burn, n_samples = params.pop('n_burn'), params.pop('n_samples')
+
+    mu = torch.randn([D])
+    sigma = torch.randn([D]).abs()
+
+    nd = NormalDist(mu, sigma)
+    Sampler = getattr(geoopt.samplers, params.pop('sampler'))
+    sampler = Sampler(nd.parameters(), **params)
+    #sampler = geoopt.samplers.RHMC(nd.parameters(), n_steps=5, epsilon=0.2)
+
+    for _ in range(n_burn):
+        sampler.step(nd)
+
+    points = []
+    sampler.burnin = False
+
+    for _ in range(n_samples):
+        sampler.step(nd)
+        points.append(nd.x.detach().numpy().copy())
+
+    points = np.asarray(points)
+    points = points[::20]
+
+    #assert points.shape == (n_samples, D)
+    np.testing.assert_allclose(mu.numpy(), points.mean(axis=0), atol=1e-1)
+    np.testing.assert_allclose(sigma.numpy(), points.std(axis=0), atol=1e-1)
