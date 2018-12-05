ad3e7ee4bf5d557369333fab1bab43f603345f1e
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-12-05 23:30:28 +0300

    fix iterable

--- geoopt/samplers/samplers.py
+++ geoopt/samplers/samplers.py
@@ -12,13 +12,6 @@ __all__ = ["RSGLD", "SGRHMC"]
 
 class Sampler(OptimMixin, optim.Optimizer):
     def __init__(self, params, defaults):
-        for param in params:
-            if isinstance(param, (ManifoldParameter, ManifoldTensor)):
-                if not param.manifold.reversible:
-                    raise ValueError(
-                        "Sampling methods can't me applied to manifolds that "
-                        "do not implement reversible retraction"
-                    )
         super().__init__(params, defaults)
         self.n_rejected = 0
         self.steps = 0
@@ -26,6 +19,14 @@ class Sampler(OptimMixin, optim.Optimizer):
 
         self.log_probs = []
         self.acceptance_probs = []
+        for group in self.param_groups:
+            for p in group["params"]:
+                if isinstance(p, (ManifoldParameter, ManifoldTensor)):
+                    if not p.manifold.reversible:
+                        raise ValueError(
+                            "Sampling methods can't me applied to manifolds that "
+                            "do not implement reversible retraction"
+                        )
 
     @property
     def rejection_rate(self):
