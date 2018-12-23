555b6d74caacacfcb2858d474e8bb5cbb3339ef1
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-12-24 02:22:13 +0300

    add test versus pymanopt

--- geoopt/util.py
+++ geoopt/util.py
@@ -16,3 +16,28 @@ def svd(x):
         return U, d, V
     else:
         return torch.svd(x)
+
+
+def broadcast_function_apply(input, output, shape, fun):
+    if not isinstance(input, (list, tuple)):
+        input = [input]
+    if isinstance(output, (list, tuple)):
+        if shape:
+            for idx in itertools.product(*map(range, shape)):
+                small_output = fun(*(inp[idx] for inp in input))
+                for out, storage in zip(small_output, output):
+                    storage[idx] = out
+        else:
+            output = output.__class__(fun(*input))
+    else:
+        if shape:
+            for idx in itertools.product(*map(range, shape)):
+                small_output = fun(*(inp[idx] for inp in input))
+                output[idx] = small_output
+        else:
+            output = fun(*input)
+    return output
+
+
+def sym(x):
+    return 0.5 * (x.transpose(-1, -2) + x)
--- tests/test_manifold.py
+++ tests/test_manifold.py
@@ -1,71 +1,86 @@
 import geoopt
-import numpy.testing
+import numpy as np
 import torch
+import collections
 import pytest
+import pymanopt.manifolds
 
 
-def test_stiefel_2d():
-    tens1 = geoopt.ManifoldTensor(20, 10, manifold=geoopt.Stiefel()).normal_().proj_()
-    vect1 = tens1.proju(torch.randn(*tens1.shape))
-    newt = tens1.retr(vect1, 1.0)
-    tens1.manifold.assert_check_point_on_manifold(newt)
+@pytest.fixture('session', params=[geoopt.manifolds.Stiefel, geoopt.manifolds.Euclidean])
+def Manifold(request):
+    return request.param
 
 
-def test_stiefel_3d():
-    tens1 = (
-        geoopt.ManifoldTensor(2, 20, 10, manifold=geoopt.Stiefel()).normal_().proj_()
-    )
-    vect1 = tens1.proju(torch.randn(*tens1.shape))
-    t = torch.randn(tens1.shape[0])
-    newt = tens1.retr(vect1, t)
-    newt_manual = list()
-    newt_manual.append(tens1.manifold.retr(tens1[0], vect1[0], t[0]))
-    newt_manual.append(tens1.manifold.retr(tens1[1], vect1[1], t[1]))
-    newt_manual = torch.stack(newt_manual)
-    numpy.testing.assert_allclose(newt_manual, newt, atol=1e-5)
-    tens1.manifold.assert_check_point_on_manifold(newt)
+mannopt = {
+    geoopt.manifolds.Stiefel: pymanopt.manifolds.Stiefel,
+    geoopt.manifolds.Euclidean: pymanopt.manifolds.Euclidean
+}
+
+# shapes to verify unary element implementation
+shapes = {
+    geoopt.manifolds.Stiefel: (10, 5),
+    geoopt.manifolds.Euclidean: (1,)
+}
+
+UnaryCase = collections.namedtuple('UnaryCase', 'shape,x,ex,v,ev,manifold,manopt_manifold')
 
 
-@pytest.mark.parametrize("Manifold", [geoopt.Stiefel, geoopt.Euclidean])
-def test_reversible_retraction(Manifold):
-    man = Manifold()
-    x = torch.randn((10,) * man.ndim)
-    x.set_(man.projx(x))
-    t = 0.1
-    u = torch.randn_like(x)
-    u.set_(man.proju(x, u))
-    xt, ut = man.retr_transp(x, u, t, u)
-    xtmt = man.retr(xt, ut, -t)
-    numpy.testing.assert_allclose(x, xtmt, atol=1e-5)
+@pytest.fixture()
+def unary_case(Manifold):
+    shape = shapes[Manifold]
+    manifold = Manifold()
+    manopt_manifold = mannopt[Manifold](*shape)
+    np.random.seed(42)
+    rand = manopt_manifold.rand()
+    x = geoopt.ManifoldTensor(torch.from_numpy(rand), manifold=manifold)
+    torch.manual_seed(43)
+    ex = geoopt.ManifoldTensor(torch.randn_like(x), manifold=manifold)
+    v = x.proju(torch.randn_like(x))
+    ev = torch.randn_like(x)
+    return UnaryCase(shape, x, ex, v, ev, manifold, manopt_manifold)
 
 
-@pytest.mark.parametrize("Manifold", [geoopt.Stiefel, geoopt.Euclidean])
-def test_transp_many(Manifold):
-    man = Manifold()
-    x = torch.randn((10,) * man.ndim)
-    x.set_(man.projx(x))
-    t = 0.1
-    u = torch.randn_like(x)
-    u.set_(man.proju(x, u))
-    vs = [man.proju(x, torch.randn_like(u)) for _ in range(3)]
-    qvs1 = man.transp(x, u, t, *vs)
-    qvs2 = tuple(man.transp(x, u, t, v) for v in vs)
-    for qv1, qv2 in zip(qvs1, qvs2):
-        numpy.testing.assert_allclose(qv1, qv2, atol=1e-5)
+def test_projection_identity(unary_case):
+    x = unary_case.x
+    xp = unary_case.manifold.projx(x)
+    np.testing.assert_allclose(x, xp)
 
 
-@pytest.mark.parametrize("Manifold", [geoopt.Stiefel, geoopt.Euclidean])
-def test_transp_many(Manifold):
-    man = Manifold()
-    x = torch.randn((10,) * man.ndim)
-    x.set_(man.projx(x))
-    t = 0.1
-    u = torch.randn_like(x)
-    u.set_(man.proju(x, u))
-    vs = [man.proju(x, torch.randn_like(u)) for _ in range(3)]
-    qvs1 = man.retr_transp(x, u, t, *vs)
-    qvs2 = tuple(man.transp(x, u, t, v) for v in vs)
-    qvs2 = (man.retr(x, u, t),) + qvs2
+def test_projection_via_assert(unary_case):
+    x = unary_case.ex
+    xp = unary_case.manifold.projx(x)
+    unary_case.manifold.assert_check_point_on_manifold(xp)
 
-    for qv1, qv2 in zip(qvs1, qvs2):
-        numpy.testing.assert_allclose(qv1, qv2, atol=1e-5)
+
+def test_vector_projection(unary_case):
+    x = unary_case.x
+    ev = unary_case.ev
+
+    pv = x.proju(ev)
+    pv_star = unary_case.manopt_manifold.egrad2rgrad(x.numpy(), ev.numpy())
+
+    np.testing.assert_allclose(pv, pv_star)
+
+
+def test_retraction(unary_case):
+    x = unary_case.x
+    v = unary_case.v
+
+    y = x.retr(v, 1.)
+    y_star = unary_case.manopt_manifold.retr(x.numpy(), v.numpy())
+
+    np.testing.assert_allclose(y, y_star)
+
+
+def test_transport(unary_case):
+    x = unary_case.x
+    v = unary_case.v
+
+    y = x.retr(v, 1.)
+
+    u = x.transp(v, 1., v)
+
+    u_star = unary_case.manopt_manifold.transp(x.numpy(), y.numpy(), v.numpy())
+
+    np.testing.assert_allclose(u, u_star)
+
