d9bf5aae054f5c3e1fb322ad2cc618ba1770d17e
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-12-28 12:25:07 +0300

    licence

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 import re
 from setuptools import setup, find_packages
 
+DESCRIPTION = """Unofficial implementation for “Riemannian Adaptive Optimization Methods” ICLR2019 and more"""
+
 
 def get_version(*path):
     version_file = os.path.join(*path)
@@ -14,12 +16,28 @@ def get_version(*path):
     raise RuntimeError("Unable to find version in %s." % (version_file,))
 
 
+classifiers = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "License :: OSI Approved :: Apache Software License",
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Mathematics",
+    "Operating System :: OS Independent",
+]
+
 if __name__ == "__main__":
     setup(
         name="geoopt",
         author="Maxim Kochurov, Victor Yanush",
+        description=DESCRIPTION,
         packages=find_packages(),
         install_requires=["torch>=0.4.1", "numpy"],
         version=get_version("geoopt", "__init__.py"),
         url="https://github.com/ferrine/geoopt",
+        python_requires=">=3.6.0",
+        licence="Apache License, Version 2.0",
+        classifiers=classifiers
     )
