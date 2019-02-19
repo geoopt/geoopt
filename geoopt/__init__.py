d845c38fc13319754a8628df39bc01994d782d19
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-02-20 02:24:50 +0300

    add import

--- geoopt/__init__.py
+++ geoopt/__init__.py
@@ -5,6 +5,6 @@ from . import samplers
 from . import linalg
 
 from .tensor import ManifoldParameter, ManifoldTensor
-from .manifolds import Stiefel, Euclidean, Sphere
+from .manifolds import Stiefel, Euclidean, Sphere, PoincareBall
 
 __version__ = "0.0.1"
