5161ce45b807fb09ff8fb53f4f77bc4307c31b57
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-02-22 01:09:46 +0300

    do not fix test

--- geoopt/optim/tracing.py
+++ geoopt/optim/tracing.py
@@ -28,4 +28,5 @@ def create_traced_update(step, manifold, point, *buffers, **kwargs):
     def partial(*args):
         step(manifold, *args, **kwargs)
         return args
+
     return torch.jit.trace(partial, (point, grad, lr) + buffers)
