9c74730ae15b323dc1bb52f30501e38c4493cf5e
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-03-03 13:06:12 +0300

    manifold .to() was not working, fix (#51)
    * sphere .to() was not working, fix
    
    * fix tests, add testing for pickle parameter+manifold

--- geoopt/manifolds/base.py
+++ geoopt/manifolds/base.py
@@ -542,7 +542,7 @@ class Manifold(torch.nn.Module, metaclass=ManifoldMeta):
         return self._retr_funcs[-1](self, x=x, u=u, t=t)
 
     def logmap(self, x, y):
-        """
+        r"""
         Perform an logarithmic map for a pair of points :math:`x` and :math:`y`.
         The result lies in :math:`u \in T_x\mathcal{M}` is such that:
 
@@ -951,6 +951,3 @@ class Manifold(torch.nn.Module, metaclass=ManifoldMeta):
             return self.name + "({}) manifold".format(extra)
         else:
             return self.name + " manifold"
-
-    def __eq__(self, other):
-        return type(self) is type(other)
--- geoopt/manifolds/sphere.py
+++ geoopt/manifolds/sphere.py
@@ -143,7 +143,7 @@ class SphereSubspaceIntersection(Sphere):
 
     def _configure_manifold(self, span):
         Q, _ = geoopt.linalg.batch_linalg.qr(span)
-        self._projector = Q @ Q.transpose(-1, -2)
+        self.register_buffer("_projector", Q @ Q.transpose(-1, -2))
 
     def _project_on_subspace(self, x):
         return x @ self._projector.transpose(-1, -2)
@@ -176,4 +176,4 @@ class SphereSubspaceComplementIntersection(SphereSubspaceIntersection):
         Q, _ = geoopt.linalg.batch_linalg.qr(span)
         P = -Q @ Q.transpose(-1, -2)
         P[..., torch.arange(P.shape[-2]), torch.arange(P.shape[-2])] += 1
-        self._projector = P
+        self.register_buffer("_projector", P)
