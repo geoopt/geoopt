bd4c2c445733d64249a1aa0e14f91c2355db2fda
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-04-10 01:12:47 +0300

    more compact Dockerfile

--- scripts/container.sh
+++ scripts/container.sh
@@ -2,7 +2,8 @@
 # prototype is taken from  https://github.com/arviz-devs/arviz/blob/master/scripts/container.sh
 SRC_DIR=${SRC_DIR:-`pwd`}
 COVERAGE=${COVERAGE:-"--cov geoopt"}
-PYTORCH=${PYTORCH:-"pytorch>=1.0.0"}
+PYTORCH=${PYTORCH:-"pytorch"}
+PYTHON_VERSION=${PYTHON_VERSION:-"3.6"}
 # Build container for use of testing or notebook
 if [[ $* == *--build* ]]; then
     echo "Building Docker Image"
@@ -11,13 +12,15 @@ if [[ $* == *--build* ]]; then
         -f ${SRC_DIR}/scripts/Dockerfile \
         --build-arg SRC_DIR=. ${SRC_DIR} \
         --build-arg PYTORCH=${PYTORCH} \
+        --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
         --rm
 fi
 
 if [[ $* == *--test* ]]; then
     echo "Testing Geoopt"
-    docker run -it --user $(id -u):$(id -g) --mount type=bind,source="$(pwd)",target=/opt/geoopt/ geoopt:latest bash -c \
-                                      "pytest --durations=0 -v tests/ ${COVERAGE}"
+    docker run -it --user $(id -u):$(id -g) --mount type=bind,source="$(pwd)",target=/opt/geoopt/ geoopt:latest \
+        bash -c "pytest --durations=0 -v tests/ ${COVERAGE}" && \
+        if [[ ${COVERAGE} ]]; then sed -i 's@/opt/geoopt@'${SRC_DIR}'@g' "$(pwd)/.coverage"; fi
 fi
 
 if [[ $* == *--bash* ]]; then
