bb0d67b1c3d6e0c05e365f970510aa28feb59326
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-05-27 14:36:57 -0400

    fix test check

--- scripts/container.sh
+++ scripts/container.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 # prototype is taken from  https://github.com/arviz-devs/arviz/blob/master/scripts/container.sh
 SRC_DIR=${SRC_DIR:-`pwd`}
 COVERAGE=${COVERAGE:-"--cov geoopt"}
@@ -19,7 +20,7 @@ fi
 if [[ $* == *--test* ]]; then
     echo "Testing Geoopt"
     docker run --rm -it --user $(id -u):$(id -g) --mount type=bind,source="$(pwd)",target=/opt/geoopt/ geoopt:latest \
-        bash -c "pytest --durations=0 -v tests/ ${COVERAGE}" && \
+        bash -c "pytest --durations=0 -v tests/ ${COVERAGE}"
         if [[ ${COVERAGE} ]]; then sed -i 's@/opt/geoopt@'${SRC_DIR}'@g' "$(pwd)/.coverage"; fi
 fi
 
