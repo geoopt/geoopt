7e56e23c33c99a8f3f6d586f8c1875b8cf59ec11
Haowen <denghaowen10@gmail.com>
2019-07-28 18:11:48 +0200

    add manifold constraint penalty

--- tests/test_birkhoff.py
+++ tests/test_birkhoff.py
@@ -13,23 +13,27 @@ def test_adam_birkhoff(params):
     birkhoff = geoopt.manifolds.BirkhoffPolytope()
     torch.manual_seed(42)
     with torch.no_grad():
-        X = geoopt.ManifoldParameter(torch.rand(10, 5, 5), manifold=birkhoff).proj_()
-    Xstar = torch.rand(10, 5, 5)
+        X = geoopt.ManifoldParameter(torch.rand(1, 5, 5), manifold=birkhoff).proj_()
+    Xstar = torch.rand(1, 5, 5)
     Xstar.set_(birkhoff.projx(Xstar))
 
-    optim = geoopt.optim.RiemannianAdam([X], stabilize=4500, **params)
-    for _ in range(10000):
+    def closure():
         optim.zero_grad()
-        loss = torch.norm(X - Xstar)
-        if loss < 1e-3:
-            break
+        loss = (X - Xstar).pow(2).sum()
+        # manifold constraint that makes optimization hard if violated
+        row_penalty = ((X.transpose(1,2) @ X).sum(dim = 1) - 1.0).pow(2).sum() * 100
+        col_penalty = ((X.transpose(1,2) @ X).sum(dim = 2) - 1.0).pow(2).sum() * 100
+        loss += row_penalty + col_penalty
         loss.backward()
-        print('{} {}'.format(_, loss))
-        optim.step()
+        return loss.item()
+
+    optim = geoopt.optim.RiemannianAdam([X], stabilize=4500, **params)
+
+    assert (X - Xstar).norm() > 1e-3
+    for _ in range(10000):
+        if (X - Xstar).norm() < 1e-3:
+            break
+        optim.step(closure)
+    assert X.is_contiguous()
 
     np.testing.assert_allclose(X.data, Xstar, atol=1e-3, rtol=1e-3)
-
-
-# if __name__ == '__main__':
-#     params = dict(lr=1e-2)
-#     test_adam_birkhoff(params)
