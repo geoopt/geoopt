4d7bcf40b6e4deb00c5a6d63b4e0b191fa68cd92
Maxim Kochurov <m.kochurov@partner.samsung.com>
2019-10-18 17:49:37 +0300

    fix lineskip

--- geoopt/manifolds/product.py
+++ geoopt/manifolds/product.py
@@ -24,6 +24,7 @@ class ProductManifold(Manifold):
     Examples
     --------
     A Torus
+
     >>> import geoopt
     >>> sphere = geoopt.Sphere()
     >>> torus = ProductManifold((sphere, 2), (sphere, 2))
