dff49ecdbeb81a396a2ed6ae3a48a8d1a7dd5153
Max Kochurov <maxim.v.kochurov@gmail.com>
2019-10-30 22:38:31 +0300

    remove some batched functions

--- geoopt/linalg/batch_linalg.py
+++ geoopt/linalg/batch_linalg.py
@@ -5,57 +5,6 @@ __all__ = ["svd", "qr", "sym", "extract_diag", "matrix_rank", "expm", "block_mat
 
 
 @torch.jit.script
-def svd(x):  # pragma: no cover
-    # inspired by
-    # https://discuss.pytorch.org/t/multidimensional-svd/4366/2
-    # prolonged here:
-    if x.dim() == 2:
-        # 17 milliseconds on my mac to check that condition, that is low overhead
-        result = torch.svd(x)
-    else:
-        batches = x.shape[:-2]
-        other = x.shape[-2:]
-        flat = x.view((-1,) + other)
-        slices = flat.unbind(0)
-        U, D, V = [], [], []
-        # I wish I had a parallel_for
-        for i in range(flat.shape[0]):
-            u, d, v = torch.svd(slices[i])
-            U += [u]
-            D += [d]
-            V += [v]
-        U = torch.stack(U).view(batches + U[0].shape)
-        D = torch.stack(D).view(batches + D[0].shape)
-        V = torch.stack(V).view(batches + V[0].shape)
-        result = U, D, V
-    return result
-
-
-@torch.jit.script
-def qr(x):  # pragma: no cover
-    # inspired by
-    # https://discuss.pytorch.org/t/multidimensional-svd/4366/2
-    # prolonged here:
-    if x.dim() == 2:
-        result = torch.qr(x)
-    else:
-        batches = x.shape[:-2]
-        other = x.shape[-2:]
-        flat = x.view((-1,) + other)
-        slices = flat.unbind(0)
-        Q, R = [], []
-        # I wish I had a parallel_for
-        for i in range(flat.shape[0]):
-            q, r = torch.qr(slices[i])
-            Q += [q]
-            R += [r]
-        Q = torch.stack(Q).view(batches + Q[0].shape)
-        R = torch.stack(R).view(batches + R[0].shape)
-        result = Q, R
-    return result
-
-
-@torch.jit.script
 def sym(x):  # pragma: no cover
     return 0.5 * (x.transpose(-1, -2) + x)
 
@@ -122,3 +71,9 @@ def block_matrix(blocks):
     # [CD]
     blocks = tuple(torch.cat(mats, dim=-1) for mats in blocks)
     return torch.cat(blocks, dim=-2)
+
+
+# left here for convenience
+qr = torch.qr
+
+svd = torch.svd
