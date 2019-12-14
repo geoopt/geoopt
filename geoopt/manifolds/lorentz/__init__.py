764a604c758ed09db52d6817e4b49f66310b5260
Rasul Karimov <yoatom96@hotmail.com>
2019-12-14 04:24:42 +0300

    initial commit on hyperboloid

---
+++ geoopt/manifolds/lorentz/__init__.py
@@ -0,0 +1,79 @@
+import torch.nn
+from typing import Tuple, Optional
+from . import math
+import geoopt
+from ..base import Manifold, ScalingInfo
+
+__all__ = ["Lorentz"]
+
+
+class Lorentz(Manifold):
+    __doc__ = r"""{}
+    """
+
+    ndim = 1
+    name = "Hyperboloid"
+
+    def __init__(self):
+        super().__init__()
+        # TODO add curvature
+
+    def _check_point_on_manifold(
+        self, x: torch.Tensor, *, atol=1e-5, rtol=1e-5
+    ) -> Tuple[bool, Optional[str]]:
+        quad_form = -x[:, -1] ** 2 + (x[:,:-1]**2).sum()
+        ok = torch.allclose(
+            quad_form, quad_form.new((1,)).fill_(-1.), atol=atol, rtol=rtol
+        )
+        if not ok:
+            reason = "'x' minkowski quadratic form is not equal to 1"
+        else:
+            reason = None
+        return ok, reason
+
+    def _check_vector_on_tangent(
+        self, x: torch.Tensor, u: torch.Tensor, *, atol=1e-5, rtol=1e-5
+    ) -> Tuple[bool, Optional[str]]:
+        return True, None
+
+    def dist(
+        self, x: torch.Tensor, y: torch.Tensor, *, keepdim=False, dim=-1
+    ) -> torch.Tensor:
+        return math.dist(x, y, keepdim=keepdim, dim=dim)
+
+    def egrad2rgrad(self, x: torch.Tensor, u: torch.Tensor, *, dim=-1) -> torch.Tensor:
+        return math.egrad2rgrad(x, u, dim=dim)
+
+    def retr(self, x: torch.Tensor, u: torch.Tensor, *, dim=-1) -> torch.Tensor:
+        approx = x + u
+        return math.project(approx, dim=dim)
+
+    def projx(self, x: torch.Tensor, dim=-1) -> torch.Tensor:
+        return math.project(x, dim=dim)
+
+    def proju():
+        target_shape = broadcast_shapes(x.shape, u.shape)
+        return u.expand(target_shape)
+
+    def expmap(
+        self, x: torch.Tensor, u: torch.Tensor, *, project=True, dim=-1
+    ) -> torch.Tensor:
+        res = math.expmap(x, u, dim=dim)
+        if project:
+            return math.project(res, dim=dim)
+        else:
+            return res
+
+    def logmap(self, x: torch.Tensor, y: torch.Tensor, *, dim=-1) -> torch.Tensor:
+        return math.logmap(x, y, c=self.c, dim=dim)
+
+
+    def inner(self, x: torch.Tensor, u: torch.Tensor, v: torch.Tensor = None, *, keepdim=False, dim=-1) -> torch.Tensor:
+        return math.inner(x, u, v)
+
+
+    def egrad2rgrad(self, x: torch.Tensor, u: torch.Tensor, *, dim=-1) -> torch.Tensor:
+        return math.egrad2rgrad(x, u, dim=dim)
+
+
+    retr = expmap
---
+++ geoopt/manifolds/lorentz/math.py
@@ -0,0 +1,147 @@
+import functools
+import torch.jit
+import torch as th
+
+MIN_NORM = 1e-15
+_eps = 1e-10
+
+
+class Arcosh(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, eps):
+        z = th.sqrt(th.clamp(x * x - 1 + eps, _eps))
+        ctx.save_for_backward(z)
+        ctx.eps = eps
+        return th.log(x + z)
+
+    @staticmethod
+    def backward(ctx, g):
+        (z,) = ctx.saved_tensors
+        z = th.clamp(z, min=ctx.eps)
+        z = g / z
+        return z, None
+
+
+class Arsinh(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x):
+        ctx.save_for_backward(x)
+        z = x.double()
+        return (z + torch.sqrt_(1 + z.pow(2))).clamp_min_(MIN_NORM).log_().to(x.dtype)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (input,) = ctx.saved_tensors
+        return grad_output / (1 + input ** 2) ** 0.5
+
+
+def arsinh(x):
+    return Arsinh.apply(x)
+
+
+def arcosh(x):
+    return Arcosh.apply(x)
+
+
+def ldot(u, v, *, dim: int = -1, keepdim=False):
+    """
+        Lorentzian Scalar Product
+    """
+    return _ldot(u, v, dim=dim, keepdim=keepdim)
+
+
+def _ldot(u, v, dim: int = -1, keepdim=False):
+    d = u.size(dim) - 1
+    uv = u * v
+    uv = th.cat((-uv.narrow(dim, 0, 1), uv.narrow(dim, 1, d)), dim=dim)
+    return th.sum(uv, dim=dim, keepdim=keepdim)
+
+
+def dist(x, y, *, keepdim=False, dim=-1, eps=1e-3):
+    """
+        docs: ...
+    """
+    return _dist(x, y, keepdim=keepdim, dim=dim, eps=eps)
+
+
+def _dist(x, y, keepdim: bool = False, dim: int = -1, eps=1e-3):
+    d = -ldot(x, y, dim=dim, keepdim=keepdim)
+    return arcosh(d, eps)
+
+
+def egrad2rgrad(x, grad, *, dim=-1):
+    """
+        docs:
+    """
+    return _egrad2rgrad(x, grad, dim=dim)
+
+
+def _egrad2rgrad(x, grad, dim: int = -1):
+    grad.narrow(dim, 0, 1).mul_(-1.)
+    grad.addcmul_(_ldot(x, grad, dim=dim, keepdim=True).expand_as(x), x)
+    return grad
+
+
+def project(x, *, dim=-1, eps=None):
+    """
+        docs
+    """
+    return _project(x, dim, eps)
+
+
+def _project(x, dim: int = -1, eps: float = None):
+    if x.dim() != 2:
+        raise RuntimeError("dimension of input should be atleast 2")
+    d = x[:, :-1] / torch.norm(x[:, :-1], dim=-1, keepdim=True)
+    r = x[:, -1][..., None]
+    return torch.cat((torch.sinh(r) * d, torch.cosh(r)), dim=-1)
+
+
+def inner(x, u, v, *, keepdim=False, dim=-1):
+    """
+        docs
+    """
+    return _inner(x, u, v, keepdim=keepdim, dim=dim)
+
+def _inner(x, u, v, keepdim: bool = False, dim: int = -1):
+    return ldot(u, v)
+
+
+def expmap(x, u, *, dim=-1):
+    """
+        docs
+    """
+    return _expmap(x, u, dim=dim)
+
+
+def _expmap(x, u, c, dim: int = -1):
+    denom = th.sqrt(ldot(u, u, keepdim=True))
+    p = (th.cosh(t) * p) + (th.sinh(t) * p / denom)
+    return p
+
+
+def logmap(x, y, *, dim=-1):
+    """
+        docs
+    """
+    return _logmap(x, y, dim=dim)
+
+
+def _logmap(x, y, *, dim: int = -1):
+    xy = ldot(x, y, keepdim=True)
+    denom = th.sqrt(xy * xy - 1)
+    v = (Acosh.apply(-xy, self.eps) / tmp ) * th.addcmul(y, xy, x)
+    return v
+
+
+def egrad2rgrad(x, grad, *, dim=-1):
+    """
+        docs
+    """
+    return _egrad2rgrad(x, grad, dim=dim)
+
+
+def _egrad2rgrad(x, grad, dim: int = -1):
+    grad.narrow(-1, 0, 1).mul_(-1)
+    grad.addcmul_(self.ldot(x, grad, keepdim=True).expand_as(x), x)
+    return grad
