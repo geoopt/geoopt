5beceff430e908c995d2393a825b864b6ff798f5
Maxim Kochurov <m.kochurov@partner.samsung.com>
2020-01-28 12:33:05 +0300

    clam min in another place

--- geoopt/manifolds/stereographic/math.py
+++ geoopt/manifolds/stereographic/math.py
@@ -1774,7 +1774,7 @@ def sproj(x: torch.Tensor, *, k: torch.Tensor, dim: int = -1):
 
 @torch.jit.script
 def _sproj(x: torch.Tensor, k: torch.Tensor, dim: int = -1):
-    inv_r = torch.sqrt(k.abs()).clamp_min(1e-15)
+    inv_r = torch.sqrt(k.abs().clamp_min(1e-15))
     factor = 1.0 / (1.0 + inv_r * x.narrow(dim, -1, 1))
     proj = factor * x.narrow(dim, 0, x.size(dim) - 1)
     return proj
@@ -1803,7 +1803,7 @@ def inv_sproj(x: torch.Tensor, *, k: torch.Tensor, dim: int = -1):
 
 @torch.jit.script
 def _inv_sproj(x: torch.Tensor, k: torch.Tensor, dim: int = -1):
-    inv_r = torch.sqrt(k.abs()).clamp_min(1e-15)
+    inv_r = torch.sqrt(k.abs().clamp_min(1e-15))
     lam_x = _lambda_x(x, k, keepdim=True, dim=dim)
     A = lam_x * x
     B = 1.0 / inv_r * (lam_x - 1.0)
