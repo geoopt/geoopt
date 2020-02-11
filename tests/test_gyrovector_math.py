cba4c50d346c68360137f30c81c656e81ba4913b
Maxim Kochurov <m.kochurov@partner.samsung.com>
2020-02-11 14:54:14 +0300

    add nan check

--- tests/test_gyrovector_math.py
+++ tests/test_gyrovector_math.py
@@ -536,12 +536,15 @@ def test_add_infinity_and_beyond(a, b, c, negative, manifold):
     for i in range(100):
         z = manifold.expmap(a, infty, project=False)
         z = manifold.projx(z)
-        assert torch.isfinite(z).all(), (i, z)
+        assert not torch.isnan(z).any(), ("Found nans", i, z)
+        assert torch.isfinite(z).all(), ("Found Infs", i, z)
         z = manifold.mobius_scalar_mul(
             torch.tensor(1000.0, dtype=z.dtype), z, project=False
         )
         z = manifold.projx(z)
-        assert torch.isfinite(z).all(), (i, z)
+        assert not torch.isnan(z).any(), ("Found nans", i, z)
+        assert torch.isfinite(z).all(), ("Found Infs", i, z)
+
         infty = manifold.transp(a, z, infty)
         assert torch.isfinite(infty).all(), (i, infty)
         a = z
